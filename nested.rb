32a662db5d990d96f48ed10f80daea1c34d359fb
Martin Milius <martin.milius@gmail.com>
2019-08-14 02:56:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,6 +22,7 @@ end
 def alan_kay_is_known_for
 	programmer_hash[:alan_kay][:known_for] = "COBOL"
 	programmer_hash{:alan_kay][:languages] = "COBOL", "FORTRAN"
+	puts programmer_hash
 	
 	programmer_hash = 
  		{
